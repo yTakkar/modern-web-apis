a790a74a3d99e995f3be5c45fd12b26cef8ffb04
Faiyaz Shaikh <www.shtakkar@gmail.com>
2020-06-13 12:26:37 +0530

    feat: add app shortcuts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect, useRef } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import logo512 from './images/logo512.png'
 import logo192 from './images/logo192.png'
@@ -13,20 +13,52 @@ const RouteLoader = Component => props => {
 
 const Home = RouteLoader(React.lazy(() => import('./pages/Home')))
 const About = RouteLoader(React.lazy(() => import('./pages/About')))
+const Profile = RouteLoader(React.lazy(() => import('./pages/Profile')))
 
 function App() {
+  const installPrompt = useRef(undefined)
+
+  useEffect(() => {
+    window.addEventListener('beforeinstallprompt', e => {
+      e.preventDefault();
+      installPrompt.current = e;
+    })
+  })
+
+  const installApp = () => {
+    if (!installPrompt.current) {
+      return;
+    }
+
+    installPrompt.current.prompt();
+    installPrompt.current.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+    });
+  }
+  
   return (
     <div className="App">
       <Router>
         <div>
-          <Link to='/' >Home</Link>
-          <Link to='/about' >About</Link>
+          <Link to='/' >Home</Link>{' '}
+          <Link to='/about' >About</Link>{' '}
+          <Link to='/profile' >Profile</Link>
+        </div>
+
+        <div>
+          <br/>
+          <button onClick={installApp} >Install app?</button>
         </div>
 
         <div>
           <Switch>
             <Route path='/' exact component={Home}  />
             <Route path='/about' exact component={About}  />
+            <Route path='/profile' component={Profile} />
           </Switch>
         </div>
 
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = props => {
+  return (
+    <div>
+      <h3>This is about page!</h3>
+    </div>
+  )
+}
+
+export default Profile;
