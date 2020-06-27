07c15a5dca460abaac90fa9c690373670bb7fbd8
Faiyaz Shaikh <www.shtakkar@gmail.com>
2020-06-27 12:51:22 +0530

    fix: register sw only dev mode also

--- src/index.js
+++ src/index.js
@@ -14,6 +14,6 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-if (process.env.NODE_ENV !== 'development') {
+// if (process.env.NODE_ENV !== 'development') {
   serviceWorker.register();
-}
+// }
