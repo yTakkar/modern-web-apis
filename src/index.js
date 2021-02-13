03298c19ea78bd7a244ee799e5c14abfbe76d3a0
Faiyaz Shaikh <www.shtakkar@gmail.com>
2021-02-13 19:23:16 +0530

    fix: enable service worker

--- src/index.js
+++ src/index.js
@@ -14,6 +14,6 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-if (false) {
+// if (false) {
   serviceWorker.register();
-}
+// }
